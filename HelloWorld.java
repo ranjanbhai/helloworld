eccd86eedf9640fe8ec33d0b569284c5a9cc399d
Ritu Ranjan Shrivastwa <ranjanbhai@users.noreply.github.com>
2016-03-30 16:47:33 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,17 +1,19 @@
+//importing packages
 import java.math.*;
 import java.io.*;
+
 class HelloWorld{
-     public String randomGenerator(String name){
-		int randomNum = 0;
+     public String randomGenerator(String name){ //function to generate random number and return with name
+	int randomNum = 0; //initialize random number variable
         int range = 10;//15-6
-        randomNum = (int)(Math.random()*range + 6); 
-		name += " "+randomNum;
-		return name;
-	 }
-	 public static void main(String []args)throws IOException{
-        HelloWorld ob = new HelloWorld();
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        String name = in.readLine();
-        System.out.println(ob.randomGenerator(name));
+        randomNum = (int)(Math.random()*range + 6); //typecasting float value from Math.random() function
+	name += " "+randomNum; //padding random number with name
+	return name; 
+     }
+      public static void main(String []args)throws IOException{
+        HelloWorld ob = new HelloWorld(); //create HelloWorld object
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in)); //stdin input
+        String name = in.readLine(); //reading name from user from command line
+        System.out.println(ob.randomGenerator(name)); //displaying output
 	}
 }
